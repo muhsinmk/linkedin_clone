818b08e5d6cf68b96d5af97f9105f38ce5f8879e
Muhsin MK <muhsinmk993@gmail.com>
2022-10-11 16:46:09 +0530

    sidebar section

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,50 @@
+import { Avatar } from "@mui/material";
 import React from "react";
+import "./sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  // recent items
+  const recentItem = (topic) => (
+    <div className="sidebar_recentItem">
+      <span className="sidebar_hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      {/* Sidebar Top  */}
+      <div className="sidebar_top">
+        <img
+          src="https://images.unsplash.com/photo-1458682625221-3a45f8a844c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bGlua2VkaW4lMjBjb3ZlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt="banner-pic."
+        />
+        <Avatar className="sidebar_avatar" />
+        <h2>Muhsin MK</h2>
+        <h4>muhsinmk@gmail.com</h4>
+      </div>
+
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed your Profile</p>
+          <p className="sidebar_statNumber">2,534</p>
+        </div>
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar_statNumber">2,356</p>
+        </div>
+      </div>
+
+      <div className="sidebar_bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
