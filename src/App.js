2458bae75bfbe125f38b96575506389b58f6a38f
Muhsin MK <muhsinmk993@gmail.com>
2022-10-14 18:13:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
-// Supporting Components 
+// Supporting Components
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       {/* App Body  */}
       <div className="app_body">
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
---
+++ src/components/Feed/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./feed.css"
+
+const index = () => {
+  return (
+    <div className="feed">
+      
+    </div>
+  )
+}
+
+export default index
